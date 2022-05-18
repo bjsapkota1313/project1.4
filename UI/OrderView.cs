1ef887aa89540a1027a110e120ae195780948a20
robytrierweiler <roby.trierweiler@energie-pass.lu>
2022-05-18 02:40:13 +0200

    Nav menu Design

--- UI/OrderView.cs
+++ UI/OrderView.cs
@@ -20,6 +20,7 @@ namespace UI
             InitializeComponent();
             control = false;
             menu_with = hamburgerMenu.Width;
+
         }
 
         private void pictureBox2_Click(object sender, EventArgs e)
@@ -29,28 +30,30 @@ namespace UI
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-            // Expand hamburger menu
-            if (control)
-            {
-                hamburgerMenu.Width = hamburgerMenu.Width + 10;
-                if (hamburgerMenu.Width >= menu_with)
+                // Expand hamburger menu
+                if (control)
                 {
-                    timer1.Stop();
-                    control = false;
-                    this.Refresh();
+                    hamburgerMenu.Width = hamburgerMenu.Width + 10;
+                    if (hamburgerMenu.Width >= menu_with)
+                    {
+                        timer1.Stop();
+                        control = false;
+                        this.Refresh();
+                    }
                 }
-            }
-            // close the hamburger menu
-            else
-            {
-                hamburgerMenu.Width = hamburgerMenu.Width - 10;
-                if (hamburgerMenu.Width <= 0)
+                // close the hamburger menu
+                else
                 {
-                    timer1.Stop();
-                    control=true;
-                    this.Refresh();
+                    hamburgerMenu.Width = hamburgerMenu.Width - 10;
+                    if (hamburgerMenu.Width <= 0)
+                    {
+                        timer1.Stop();
+                        control = true;
+                        this.Refresh();
+                        hamburgerMenu.Visible = true;
+
+                    }
                 }
-            }
         }
 
         private void OrderView_Load(object sender, EventArgs e)
