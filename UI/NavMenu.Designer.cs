6a702314eb4fd993d600df71c9c7b8e42348959c
robytrierweiler <roby.trierweiler@energie-pass.lu>
2022-05-18 02:43:00 +0200

    Revert "Revert "Nav menu Design""
    This reverts commit 22d18b3e133a054e5acf2c8f5bd3f960b3241bfd.

--- UI/NavMenu.Designer.cs
+++ UI/NavMenu.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace UI
 {
-    partial class OrderView
+    partial class NavMenu
     {
         /// <summary>
         /// Required designer variable.
@@ -28,30 +28,48 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(OrderView));
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(NavMenu));
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.BackBtnOrder = new System.Windows.Forms.Button();
             this.hamburgerMenu = new System.Windows.Forms.Panel();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.button1 = new System.Windows.Forms.Button();
-            this.panel2 = new System.Windows.Forms.Panel();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
-            this.timer1 = new System.Windows.Forms.Timer(this.components);
-            this.hamburgerMenu.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.panel2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.hamburgerMenu.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.DimGray;
+            this.panel2.Controls.Add(this.pictureBox2);
+            this.panel2.Controls.Add(this.BackBtnOrder);
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(728, 68);
+            this.panel2.TabIndex = 4;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(672, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(44, 36);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
             // 
             // BackBtnOrder
             // 
             this.BackBtnOrder.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(254)))), ((int)(((byte)(166)))), ((int)(((byte)(143)))));
             this.BackBtnOrder.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.BackBtnOrder.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.BackBtnOrder.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.BackBtnOrder.ForeColor = System.Drawing.Color.Snow;
             this.BackBtnOrder.Location = new System.Drawing.Point(12, 12);
             this.BackBtnOrder.Name = "BackBtnOrder";
-            this.BackBtnOrder.Size = new System.Drawing.Size(172, 36);
+            this.BackBtnOrder.Size = new System.Drawing.Size(105, 23);
             this.BackBtnOrder.TabIndex = 0;
             this.BackBtnOrder.Text = "Back to Home";
             this.BackBtnOrder.UseVisualStyleBackColor = false;
@@ -65,7 +83,7 @@
             this.hamburgerMenu.Location = new System.Drawing.Point(528, 0);
             this.hamburgerMenu.Name = "hamburgerMenu";
             this.hamburgerMenu.Size = new System.Drawing.Size(200, 1061);
-            this.hamburgerMenu.TabIndex = 1;
+            this.hamburgerMenu.TabIndex = 3;
             // 
             // pictureBox1
             // 
@@ -88,33 +106,7 @@
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
             // 
-            // panel2
-            // 
-            this.panel2.BackColor = System.Drawing.Color.DimGray;
-            this.panel2.Controls.Add(this.pictureBox2);
-            this.panel2.Controls.Add(this.BackBtnOrder);
-            this.panel2.Location = new System.Drawing.Point(0, 0);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(728, 68);
-            this.panel2.TabIndex = 2;
-            // 
-            // pictureBox2
-            // 
-            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
-            this.pictureBox2.Location = new System.Drawing.Point(672, 12);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(44, 36);
-            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox2.TabIndex = 1;
-            this.pictureBox2.TabStop = false;
-            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
-            // 
-            // timer1
-            // 
-            this.timer1.Interval = 10;
-            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
-            // 
-            // OrderView
+            // NavMenu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
@@ -122,25 +114,24 @@
             this.ClientSize = new System.Drawing.Size(728, 1061);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.hamburgerMenu);
-            this.Name = "OrderView";
-            this.Text = "Orders";
-            this.Load += new System.EventHandler(this.OrderView_Load);
-            this.hamburgerMenu.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.Name = "NavMenu";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.NavMenu_Load);
             this.panel2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.hamburgerMenu.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.PictureBox pictureBox2;
         private System.Windows.Forms.Button BackBtnOrder;
         private System.Windows.Forms.Panel hamburgerMenu;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Panel panel2;
-        private System.Windows.Forms.PictureBox pictureBox2;
-        private System.Windows.Forms.Timer timer1;
     }
 }
